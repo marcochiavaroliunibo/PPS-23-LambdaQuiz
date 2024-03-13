c81b89cb3b80acb6bf09a14c591e0b35ae916fbc
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-13 13:57:26 +0100

    Aggiunti test per verificare l'interazione tra la classe utente ed il database

--- src/test/scala/it/unibo/pps/Test.scala
+++
@@ -1,5 +0,0 @@
-package it.unibo.pps
-
-class Test {
-
-}
---
+++ src/test/scala/it/unibo/pps/UserRepositoryTests.scala
@@ -0,0 +1,28 @@
+package it.unibo.pps
+
+import it.unibo.pps.business.ConnectionMongoDB
+import it.unibo.pps.business.users.UserRepository
+import it.unibo.pps.model.User
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.*
+
+class UserRepositoryTests extends AsyncFlatSpec with should.Matchers:
+  "The application" should "connect to the database" in {
+    ConnectionMongoDB.getDatabase.map(db => db.name shouldEqual "LambdaQuiz")
+  }
+
+  val user = new User("albisyx", "Password!")
+  "A user" should "eventually be inserted in the database" in {
+    val userRepository = new UserRepository
+    userRepository.create(user)
+      .map(_ shouldBe a [Unit])
+  }
+
+  it should "be read from the database" in {
+    val userRepository = new UserRepository
+    val futureUser = userRepository.read(user.getID)
+    futureUser
+      .map(_.exists(_.getName == "albisyx") should be (true))
+  }
+end UserRepositoryTests
+
