9e4d358322be9c1e938ea4f29c1774ae5e904b1a
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-13 13:55:08 +0100

    Classe User opportunamente modificata in modo che possa essere scritta e letta dal database

---
+++ src/main/scala/it/unibo/pps/business/users/UserRepository.scala
@@ -0,0 +1,35 @@
+package it.unibo.pps.business.users
+
+import it.unibo.pps.business.{ConnectionMongoDB, Repository}
+import it.unibo.pps.model.User
+import reactivemongo.api.bson.BSONDocument
+import reactivemongo.api.bson.collection.BSONCollection
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+class UserRepository extends Repository[User]:
+  protected val collection: Future[BSONCollection] = ConnectionMongoDB.getDatabase.map(_.collection("users"))
+
+  override def create(user: User): Future[Unit] =
+    this.collection
+      .map(_.insert.one(user))
+      .map(_.onComplete {
+        case Success(wr) => println("wr.code.get " + wr)
+        case Failure(f) => f.printStackTrace()
+      })
+
+  override def read(id: String): Future[Option[User]] =
+    this.collection
+      .map(_.find(BSONDocument(
+        "_id" -> id
+      )).one[User])
+      .flatMap(_.andThen {
+        case Failure(e) => e.printStackTrace()
+        case Success(Some(u)) => Some(u)
+        case Success(None) => None
+      })
+
+end UserRepository
+
