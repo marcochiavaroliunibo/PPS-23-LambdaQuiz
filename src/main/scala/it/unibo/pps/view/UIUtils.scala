e40af30ca8e23e28af4fed9894de524f9c2aea47
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-15 19:06:01 +0100

    Aggiunto metodo per cambiare la scena corrente con una nuova

--- src/main/scala/it/unibo/pps/view/UIUtils.scala
+++ src/main/scala/it/unibo/pps/view/UIUtils.scala
@@ -1,12 +1,18 @@
 package it.unibo.pps.view
 
+import javafx.stage.Stage
+import scalafx.scene.Scene
+import scalafx.Includes.*
 import scalafx.geometry.Insets
 import scalafx.scene.layout.{Background, BackgroundFill, CornerRadii}
 import scalafx.scene.paint.Paint
 
 object UIUtils:
 
-  def craftBackground(paint: Paint): Background =
-    new Background(Array(new BackgroundFill(paint, CornerRadii.Empty, Insets(4))))
+  def craftBackground(paint: Paint, insets: Int = 0): Background =
+    new Background(Array(new BackgroundFill(paint, CornerRadii.Empty, Insets(insets))))
+
+  def changeScene(currentScene: Scene, newScene: => Scene): Unit =
+    currentScene.window.value.asInstanceOf[Stage].scene = newScene
 
 end UIUtils
