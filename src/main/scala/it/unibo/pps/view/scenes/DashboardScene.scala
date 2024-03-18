d3f2a5ef6f5e39715549ab87eec3ca43ee0ac7ca
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-18 12:48:00 +0100

    Prova della stampa degli utenti autenticati

--- src/main/scala/it/unibo/pps/view/scenes/DashboardScene.scala
+++ src/main/scala/it/unibo/pps/view/scenes/DashboardScene.scala
@@ -1,17 +1,32 @@
 package it.unibo.pps.view.scenes
 
-import scalafx.beans.property.StringProperty
+import it.unibo.pps.controller.UserController
+import it.unibo.pps.view.UIUtils
+import scalafx.geometry.Pos
 import scalafx.scene.Scene
-import scalafx.scene.layout.{BorderPane, VBox}
+import scalafx.scene.layout.*
+import scalafx.scene.paint.Color
 import scalafx.scene.text.Text
 
-import scala.concurrent.{ExecutionContext, Future}
-
-class DashboardScene(val future: Future[Int]) extends Scene:
-    root = new BorderPane {
-    top = new Text("Dashboard")
-    center = new VBox {
-      children = new Text("matches list")
+class DashboardScene extends Scene:
+  root = new BorderPane {
+    top = new Text("Dashboard") {
+      alignmentInParent = Pos.Center
+      font = UIUtils.getSceneTitleFont
+    }
+    center = new HBox(100) {
+      alignment = Pos.TopCenter
+      children = UserController.getLoggedUsers
+        .getOrElse(List.empty)
+        .map(user =>
+          new VBox(10) {
+            alignment = Pos.TopRight
+            children = new Text {
+              text = user.getUsername
+              style = "-fx-font: normal bold 18pt sans-serif"
+            }
+          }
+        )
     }
   }
 end DashboardScene
