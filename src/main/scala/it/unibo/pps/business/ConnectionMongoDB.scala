bc4af962c4c6313763271bae946b52444841347a
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-18 14:12:06 +0100

    Piccolo refactoring che permette di instaurare la connessione con il db a richiesta

--- src/main/scala/it/unibo/pps/business/ConnectionMongoDB.scala
+++ src/main/scala/it/unibo/pps/business/ConnectionMongoDB.scala
@@ -3,6 +3,7 @@ package it.unibo.pps.business
 import org.slf4j.{Logger, LoggerFactory}
 import reactivemongo.api.{AsyncDriver, DB, MongoConnection}
 
+import scala.compiletime.uninitialized
 import scala.concurrent.Future
 
 /** Questo object si occupa di fornire i servizi di connessione al database MongoDB
@@ -17,19 +18,26 @@ object ConnectionMongoDB {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  logger.info(s"Trying to connect to $databaseName database...")
-  private val database = for
-    mongoUri <- MongoConnection.fromString(connectionString)
-    connection <- mongoDriver.connect(mongoUri)
-    db <- connection.database(databaseName)
-  yield db
+  private var database: Future[DB] = uninitialized
 
-  database.onComplete {
-    case resolution => logger.info(s"Successfully connected to $databaseName database")
-    case null       => logger.info(s"Error while connecting to $databaseName database")
-  }
+  def initiateDatabaseConnection(): Unit =
+    logger.info(s"Trying to connect to $databaseName database...")
+    database = for
+      mongoUri <- MongoConnection.fromString(connectionString)
+      connection <- mongoDriver.connect(mongoUri)
+      db <- connection.database(databaseName)
+    yield db
+
+    import scala.util.{Success, Failure}
+    database.onComplete {
+      case Success(db) =>
+        logger.info(s"Successfully connected to ${db.name} database")
+      case Failure(ex) =>
+        logger.error(s"Error while connecting to $databaseName database", ex)
+    }
 
   def getDatabase: Future[DB] = database
 
   def closeConnection(): Unit = mongoDriver.close()
+
 }
