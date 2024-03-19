4e5fbf727b2103b9b6439c1dbf3b23b1e79471bf
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-19 14:22:23 +0100

    Piccolo refactoring

--- src/main/scala/it/unibo/pps/view/scenes/DashboardScene.scala
+++ src/main/scala/it/unibo/pps/view/scenes/DashboardScene.scala
@@ -2,6 +2,7 @@ package it.unibo.pps.view.scenes
 
 import it.unibo.pps.controller.UserController
 import it.unibo.pps.view.UIUtils
+import it.unibo.pps.view.components.CurrentGameStatus
 import scalafx.geometry.Pos
 import scalafx.scene.Scene
 import scalafx.scene.layout.*
@@ -10,23 +11,11 @@ import scalafx.scene.text.Text
 
 class DashboardScene extends Scene:
   root = new BorderPane {
+    background = UIUtils.defaultBackground
     top = new Text("Dashboard") {
       alignmentInParent = Pos.Center
       font = UIUtils.getSceneTitleFont
     }
-    center = new HBox(100) {
-      alignment = Pos.TopCenter
-      children = UserController.getLoggedUsers
-        .getOrElse(List.empty)
-        .map(user =>
-          new VBox(10) {
-            alignment = Pos.TopRight
-            children = new Text {
-              text = user.getUsername
-              style = "-fx-font: normal bold 18pt sans-serif"
-            }
-          }
-        )
-    }
+    center = CurrentGameStatus()
   }
 end DashboardScene
