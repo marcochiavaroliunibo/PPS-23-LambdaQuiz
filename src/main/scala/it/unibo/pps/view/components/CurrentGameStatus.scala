e543c264ea352087f2a37477e24088df5d63b1f0
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-19 19:16:51 +0100

    bozza degli elementi grafici per visualizzare i risultati dei rounds

--- src/main/scala/it/unibo/pps/view/components/CurrentGameStatus.scala
+++ src/main/scala/it/unibo/pps/view/components/CurrentGameStatus.scala
@@ -1,32 +1,45 @@
 package it.unibo.pps.view.components
 
-import it.unibo.pps.controller.{GameController, RoundController, UserController}
+import it.unibo.pps.controller.{GameController, UserController}
 import it.unibo.pps.view.UIUtils
 import scalafx.geometry.{Insets, Orientation, Pos}
 import scalafx.scene.control.{Label, Separator}
 import scalafx.scene.layout.*
 import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
 
 private class CurrentGameStatus extends HBox(10):
   private val loggedUsers = UserController.getLoggedUsers.getOrElse(List.empty)
   private val currentGame = GameController.getCurrentGameFromPlayers(loggedUsers)
-  private val roundsOfCurrentGame = 0
-
-  prefWidth = scene.get().getWindow.getWidth * 0.5
-  prefHeight = scene.get().getWindow.getHeight * 0.5
-  background = UIUtils.craftBackground(Color(80, 80, 80, 1), 4)
-  border = new Border(
-    new BorderStroke(Color(80, 80, 80, 1), BorderStrokeStyle.Solid, CornerRadii(4), BorderWidths.Empty)
+  case class RounD(p1: List[Int], p2: List[Int])
+  private val roundsOfCurrentGameMockup = Seq(
+    RounD(List(1, 1, 0), List(1, 1, 0)),
+    RounD(List(0, 0, 0), List(0, 1, 0)),
+    RounD(List(1, 1, 1), List(0, 0, 1))
   )
+
+  margin = Insets(3)
+  background = UIUtils.craftBackground(Color.web("#707070"), 5)
   alignment = Pos.TopCenter
   children = loggedUsers
     .map(user =>
       new VBox {
+        hgrow = Priority.Always
+        alignment = Pos.TopCenter
         children = Seq(
           new Label(user.getUsername) { style = "-fx-font: normal bold 24px serif" },
+          new Label(s"Punti parziali: ${currentGame.getOrElse(0)}") { style = "-fx-font: normal bold 20px serif" },
           new Separator { orientation = Orientation.Horizontal; margin = Insets(5, 0, 5, 0) },
           // qui ci saranno tutti i dati relativi alla partita corrente
-          new Label(s"Punti parziali: $currentGame") { style = "-fx-font: normal bold 24px serif" }
+          new Label("Rounds") { style = "-fx-font: normal bold 20px serif" },
+          new VBox(5) {
+            alignment = Pos.TopCenter
+            children = roundsOfCurrentGameMockup.map(round =>
+              new HBox(2) {
+                children = round.p1.map(i => Rectangle(30, 20, if i == 1 then Color.Green else Color.Red))
+              }
+            )
+          }
         )
       }
     )
