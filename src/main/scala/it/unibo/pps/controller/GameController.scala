1b5fc15b260b5075625c0e9bee3f693f00d38da0
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-19 14:24:40 +0100

    Ampliamento del game Controller

--- src/main/scala/it/unibo/pps/controller/GameController.scala
+++ src/main/scala/it/unibo/pps/controller/GameController.scala
@@ -1,22 +1,32 @@
 package it.unibo.pps.controller
 
-import it.unibo.pps.business.RoundRepository
-import it.unibo.pps.model.{Game, Round, User}
+import it.unibo.pps.business.{GameRepository, RoundRepository}
+import it.unibo.pps.model.{Game, User}
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.*
 
 object GameController:
 
-  private var game: Game = null
+  private var _gameOfLoggedUsers: Option[Game] = None
+  private val gameRepository = new GameRepository
   private val roundRepository = new RoundRepository
 
-  def getLastRoundByGame: Round = {
-    val roundResult = Await.result(roundRepository.getLastRoundByGame(game), Duration.Inf)
-    if roundResult.isDefined then roundResult.get else null
-  }
+//  def getLastRoundByGame: Round = {
+//    val roundResult = Await.result(roundRepository.getLastRoundByGame(game), 5.seconds)
+//    if roundResult.isDefined then roundResult.get else null
+//  }
 
-  def setGame(newGame: Game): Unit = game = newGame
-  def getGame: Game = game
+  def gameOfLoggedUsers: Option[Game] = _gameOfLoggedUsers
+  private def gameOfLoggedUsers_=(g: Game): Unit = _gameOfLoggedUsers = Some(g)
+
+  def getCurrentGameFromPlayers(users: List[User]): Game =
+    gameOfLoggedUsers.getOrElse {
+      gameOfLoggedUsers = Await.result(gameRepository.getCurrentGameFromPlayers(users), 5.seconds).get
+      gameOfLoggedUsers.get
+    }
+
+//  def computePartialPointsOfUser(u: User): Int = 
+//    Await.result(roundRepository.getAllRoundsByGame(gameOfLoggedUsers.get), 5.seconds).
 
 end GameController
