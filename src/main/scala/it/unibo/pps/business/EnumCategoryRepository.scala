875330f6d40d4ab1af4c9a7e646d40d7b657daba
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-20 16:53:54 +0100

    Aggiunta versione dell'entità "Category" sfruttando gli enum di Scala 3.

---
+++ src/main/scala/it/unibo/pps/business/EnumCategoryRepository.scala
@@ -0,0 +1,23 @@
+package it.unibo.pps.business
+
+import it.unibo.pps.model.EnumCategory
+import reactivemongo.api.bson.BSONDocument
+import reactivemongo.api.bson.collection.BSONCollection
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class EnumCategoryRepository extends Repository[EnumCategory]:
+  override val collection: Future[BSONCollection] = ConnectionMongoDB.getDatabase.map(_.collection("enums"))
+  
+  override def read(id: String): Future[Option[EnumCategory]] = ???
+  
+  def insertCategory(c: EnumCategory): Future[Unit] =
+    create(c)
+    
+  def getCategoryFromName(name: String): Future[Option[EnumCategory]] =
+    readOne(BSONDocument(
+      "name" -> name
+    ))
+end EnumCategoryRepository
+
