5116971c9af5638417a8a83b94e7369e1188d16d
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-21 09:44:51 +0100

    Test aggiornato in modo che copra più casistiche dello UserController

--- src/test/scala/it/unibo/pps/controller/UserControllerTests.scala
+++ src/test/scala/it/unibo/pps/controller/UserControllerTests.scala
@@ -1,10 +1,10 @@
 package it.unibo.pps.controller
 
 import it.unibo.pps.model.User
-import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.*
 
-class UserControllerTests extends AsyncFlatSpec with should.Matchers:
+class UserControllerTests extends AnyFlatSpec with should.Matchers:
 
   private val users: List[User] = (1 to 2).map(i => new User(s"UserTest$i", s"password$i")).toList
 
@@ -12,8 +12,21 @@ class UserControllerTests extends AsyncFlatSpec with should.Matchers:
     users.map(UserController.registerUser) shouldBe a[List[Unit]]
   }
 
-  it should "be able to log in users" in {
+  it should "not let users to log in if they are both unregistered" in {
+    val unregisteredUsers = users.map(u => new User(u.password, u.username))
+    UserController.checkLogin(unregisteredUsers) should be(false)
+    UserController.loggedUsers.nonEmpty should be(false)
+  }
+
+  it should "not let users to log in if one of them is unregistered" in {
+    val unregisteredUser = users.map(u => new User(u.password, u.username)).take(1)
+    UserController.checkLogin(users.head :: unregisteredUser) should be(false)
+    UserController.loggedUsers.nonEmpty should be(false)
+  }
+
+  it should "let registered users to log in" in {
     UserController.checkLogin(users) should be(true)
+    UserController.loggedUsers.nonEmpty should be(true)
   }
 
 end UserControllerTests
