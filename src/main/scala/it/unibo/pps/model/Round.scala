257a0abd30ef46224158de548739d168c54506a9
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-03-22 19:26:38 +0100

    Ora il round contiene una lista di scores

--- src/main/scala/it/unibo/pps/model/Round.scala
+++ src/main/scala/it/unibo/pps/model/Round.scala
@@ -8,8 +8,7 @@ import scala.util.Try
 
 case class Round(
     private val _relatedGameID: String,
-    var pointUser1: Int,
-    var pointUser2: Int,
+    scores: List[Score],
     numberRound: Int,
     id: Option[UUID] = None
 ) {
@@ -18,20 +17,18 @@ case class Round(
 
   def getID: String = _id.toString
   def relatedGameID: String = _relatedGameID
-  def getPoint1: Int = pointUser1
-  def getPoint2: Int = pointUser2
   def getNumberRound: Int = numberRound
 
   /** set punteggio di un player -1: non ha mai giocato il round 0: ha giocato il round ma non ha mai indovinato la
     * risposta 1+ ha giocato il round e indovinato 1+ risposte:
     */
-  def setPoint(numberPlayer: Int, correct: Boolean): Unit =
-    if numberPlayer == 1 then
-      if getPoint1 == -1 then pointUser1 = 0
-      if correct then pointUser1 = pointUser1 + 1
-    else
-      if getPoint2 == -1 then pointUser2 = 0
-      if correct then pointUser2 = pointUser2 + 1
+//  def setPoint(numberPlayer: Int, correct: Boolean): Unit =
+//    if numberPlayer == 1 then
+//      if getPoint1 == -1 then pointUser1 = 0
+//      if correct then pointUser1 = pointUser1 + 1
+//    else
+//      if getPoint2 == -1 then pointUser2 = 0
+//      if correct then pointUser2 = pointUser2 + 1
 }
 
 object Round {
@@ -40,24 +37,21 @@ object Round {
       for
         id <- doc.getAsTry[String]("_id")
         gameID <- doc.getAsTry[String]("gameID")
-        pointUser1 <- doc.getAsTry[Int]("pointUser1")
-        pointUser2 <- doc.getAsTry[Int]("pointUser2")
+        scores <- doc.getAsTry[List[Score]]("scores")
         numberRound <- doc.getAsTry[Int]("numberRound")
-      yield Round(gameID, pointUser1, pointUser2, numberRound, Some(UUID.fromString(id)))
+      yield Round(gameID, scores, numberRound, Some(UUID.fromString(id)))
 
   implicit object RoundWriter extends BSONDocumentWriter[Round]:
     override def writeTry(round: Round): Try[BSONDocument] =
       for
         id <- Try(round.getID)
         gameID <- Try(round.relatedGameID)
-        pointUser1 <- Try(round.getPoint1)
-        pointUser2 <- Try(round.getPoint2)
+        scores <- Try(round.scores)
         numberRound <- Try(round.getNumberRound)
       yield BSONDocument(
         "_id" -> id,
         "gameID" -> gameID,
-        "pointUser1" -> pointUser1,
-        "pointUser2" -> pointUser2,
+        "scores" -> scores,
         "numberRound" -> numberRound
       )
 }
---
+++ src/main/scala/it/unibo/pps/model/Score.scala
@@ -0,0 +1,26 @@
+package it.unibo.pps.model
+
+import reactivemongo.api.bson.{BSONDocument, BSONDocumentReader, BSONDocumentWriter}
+
+import scala.util.Try
+
+case class Score(user: User, score: Int)
+
+object Score:
+  implicit object ScoreReader extends BSONDocumentReader[Score]:
+    def readDocument(doc: BSONDocument): Try[Score] =
+      for
+        user <- doc.getAsTry[User]("users")
+        score <- doc.getAsTry[Int]("score")
+      yield Score(user, score)
+
+  implicit object ScoreWriter extends BSONDocumentWriter[Score]:
+    override def writeTry(score: Score): Try[BSONDocument] =
+      for
+        user <- Try(score.user)
+        score <- Try(score.score)
+      yield BSONDocument(
+        "user" -> user,
+        "score" -> score
+      )
+end Score
