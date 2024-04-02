7c75b89c2bae78b8d5827c1792e011233f0157ac
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-04-02 16:08:58 +0200

    Ora quando non vi è una partita in corso, viene mostrato un messaggio per informare l'utente

--- src/main/scala/it/unibo/pps/view/components/CurrentGameStatus.scala
+++ src/main/scala/it/unibo/pps/view/components/CurrentGameStatus.scala
@@ -1,7 +1,7 @@
 package it.unibo.pps.view.components
 
 import it.unibo.pps.controller.{QuestionController, RoundController}
-import it.unibo.pps.model.{Game, Round, User}
+import it.unibo.pps.model.{Game, User}
 import it.unibo.pps.view.UIUtils
 import scalafx.geometry.{Insets, Orientation, Pos}
 import scalafx.scene.control.{Label, Separator}
@@ -21,40 +21,50 @@ private class CurrentGameStatus(currentGame: Option[Game]) extends HBox(10):
   alignment = Pos.TopCenter
   children = currentGame
     .map(_.players)
-    .getOrElse(List.empty)
-    .map(user =>
-      new VBox {
-        hgrow = Priority.Always
-        alignment = Pos.TopCenter
-        children = Seq(
-          new Label(user.username) { style = "-fx-font: normal bold 24px serif" },
-          new Label(s"Punti parziali: ${partialPointsOfUser(user)}") { style = "-fx-font: normal bold 20px serif" },
-          new Separator { orientation = Orientation.Horizontal; margin = Insets(5, 0, 5, 0) },
-          // qui ci saranno tutti i dati relativi alla partita corrente
-          new Label("Rounds") { style = "-fx-font: normal bold 20px serif" },
-          new VBox(5) {
-            alignment = Pos.TopCenter
-            children = playedRounds match
-              case rounds: List[Round] =>
-                rounds.map(round =>
+    .map(
+      _.map(user =>
+        new VBox {
+          hgrow = Priority.Always
+          alignment = Pos.TopCenter
+          children = Seq(
+            new Label(user.username) { style = "-fx-font: normal bold 24px serif" },
+            new Label(s"Punti parziali: ${partialPointsOfUser(user)}") { style = "-fx-font: normal bold 20px serif" },
+            new Separator { orientation = Orientation.Horizontal; margin = Insets(5, -5, 5, -5) },
+            // qui ci saranno tutti i dati relativi alla partita corrente
+            new Label("Rounds") { style = "-fx-font: normal bold 20px serif" },
+            new VBox(5) {
+              alignment = Pos.TopCenter
+              children = playedRounds
+                .getOrElse(List.empty)
+                .zipWithIndex
+                .map((round, i) =>
                   new HBox(2) {
-                    val score: Seq[Int] = round.scores.filter(_.user == user).filter(_.score != -1).map(s => s.score)
-                    var listBox: List[Rectangle] = List()
-                    if score.nonEmpty then
-                      /** aggiungo box verdi per ogni domanda indovinata dall'utente nel round */
-                      for (i <- 1 to score.head)
-                        listBox = Rectangle(30, 20, Color.Green) :: listBox
-                      /** aggiungo box rossi per ogni domanda sbagliata dall'utente nel round */
-                      for (i <- listBox.length + 1 to QuestionController.QUESTION_FOR_ROUND)
-                        listBox = Rectangle(30, 20, Color.Red) :: listBox
-                      children = listBox
+                    children = new Text(s"${i + 1})  ") :: round.scores
+                      .filter(_.user == user)
+                      .filter(_.score != -1)
+                      .flatMap(s =>
+                        var listBox: List[Rectangle] = List.empty
+
+                        /** aggiungo box verdi per ogni domanda indovinata dall'utente nel round */
+                        for (i <- 1 to s.score)
+                          listBox = Rectangle(30, 20, Color.Green) :: listBox
+
+                        /** aggiungo box rossi per ogni domanda sbagliata dall'utente nel round */
+                        for (i <- listBox.length + 1 to QuestionController.QUESTION_FOR_ROUND)
+                          listBox = Rectangle(30, 20, Color.Red) :: listBox
+                        listBox
+                      )
                   }
                 )
-              case null => List(new Text("Non c'è alcuna partita in corso al momento!"))
-          }
-        )
-      }
+            }
+          )
+        }
+      )
     )
+    .getOrElse(List(new Text("Non c'è alcuna partita in corso al momento!") {
+      style = "-fx-font: normal bold 22px serif"
+      margin = Insets(10)
+    }))
 end CurrentGameStatus
 
 object CurrentGameStatus:
