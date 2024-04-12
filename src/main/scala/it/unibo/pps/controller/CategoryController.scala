0fa8c67bbdf9dcecf8ca3f1d060d8cea1f69add0
marcochiavaroliunibo <marco.chiavaroli@studio.unibo.it>
2024-04-12 19:13:29 +0200

    categorie non ripetute in stesso game

--- src/main/scala/it/unibo/pps/controller/CategoryController.scala
+++ src/main/scala/it/unibo/pps/controller/CategoryController.scala
@@ -9,9 +9,13 @@ object CategoryController:
   /** estrae casualmente un numero di categorie pari al numero di round giocati per ogni game */
   def getRandomCategories(roundForGame: Int): List[Category] =
     var listCategories: List[Category] = List()
-    val sizeCategory: Int = Category.values.length 
+    val sizeCategory: Int = Category.values.length
+    val listNumber = List()
     for (i <- 1 to roundForGame) {
-      listCategories = Category.values(Random.nextInt(sizeCategory)) :: listCategories
+      var random: Int  = Random.nextInt(sizeCategory)
+      while (listNumber.contains(random))
+        random = Random.nextInt(sizeCategory)
+      listCategories = Category.values(random) :: listCategories
     }
     listCategories
 
