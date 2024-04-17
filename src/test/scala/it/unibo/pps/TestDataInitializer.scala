dc544be2a581cc2bc5797a61d3072fd844f3b5d4
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-04-17 16:43:48 +0200

    Generazione casuale dei dati su cui effettuare i vari test

--- src/test/scala/it/unibo/pps/TestDataInitializer.scala
+++ src/test/scala/it/unibo/pps/TestDataInitializer.scala
@@ -6,9 +6,10 @@ import it.unibo.pps.model.*
 import it.unibo.pps.model.Category.Storia
 import reactivemongo.api.bson.BSONDocument
 
-import java.time.LocalDateTime
+import java.time.{LocalDateTime, Month}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.Random
 
 object TestDataInitializer:
   val gameRepository = new GameRepository
@@ -16,16 +17,10 @@ object TestDataInitializer:
   val roundRepository = new RoundRepository
   val questionRepository = new QuestionRepository
 
-  val categories: List[Category] = CategoryController.getRandomCategories(3)
+  def categories: List[Category] = CategoryController.getRandomCategories(3)
   val players: List[User] = (1 to 2).map(i => User(s"User$i", s"passwd${i + 1}")).toList
-  val game: Game = Game(
-    players,
-    false,
-    LocalDateTime.now(),
-    categories
-  )
-  private val scoreList = players.zipWithIndex.map((user, index) => Score(user, index + 1))
-  val round: Round = Round(game.id, scoreList, 2)
+  val games: List[Game] = generateGames
+  val rounds: List[Round] = generateRounds
   val question: Question = Question(
     "Chi è stato il primo presidente degli Stati Uniti?",
     List("Thomas Jefferson", "Abraham Lincoln", "John Adams", "George Washington"),
@@ -33,13 +28,43 @@ object TestDataInitializer:
     Storia
   )
 
+  private def generateGames: List[Game] =
+    val currentGame: Game = Game(
+      players,
+      false,
+      LocalDateTime.now(),
+      categories
+    )
+    val completedGames =
+      (1 to 5)
+        .map(_ => Game(players, true, generateRandomDateTime, categories))
+        .toList
+    currentGame :: completedGames
+
+  private def generateRounds: List[Round] =
+    games.flatMap { game =>
+      (1 to 3).map { i =>
+        val scoreList = game.players.zipWithIndex.map { case (user, index) =>
+          Score(user, Random.between(0, 3))
+        }
+        Round(game.id, scoreList, i)
+      }
+    }
+
+  private def generateRandomDateTime: LocalDateTime =
+    val year = Random.between(2020, 2024)
+    val month = Random.between(1, 12)
+    val day = Random.between(1, 28)
+    val hour = Random.between(0, 23)
+    val minute = Random.between(0, 59)
+    LocalDateTime.of(year, month, day, hour, minute)
+
   def initData: Future[Unit] =
     Future
       .sequence(players.map(userRepository.create))
+      .flatMap(_ => Future.sequence(games.map(gameRepository.create)))
+      .flatMap(_ => Future.sequence(rounds.map(roundRepository.create)))
       .map(_ => {})
-      .andThen(_ => gameRepository.create(game))
-      .andThen(_ => roundRepository.create(round))
-      .andThen(_ => questionRepository.create(question))
 
   def cleanData(): Future[Unit] =
     val selectAll = BSONDocument()
@@ -47,7 +72,7 @@ object TestDataInitializer:
       .delete(selectAll)
       .andThen(_ => gameRepository.delete(selectAll))
       .andThen(_ => roundRepository.delete(selectAll))
-      .andThen(_ => questionRepository.delete(selectAll))
+//    .andThen(_ => questionRepository.delete(selectAll))
       .andThen(_ => Thread.sleep(10))
 
 end TestDataInitializer
