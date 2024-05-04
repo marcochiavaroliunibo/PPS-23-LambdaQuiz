0c1e640f6c423f8ef9dae3e3189583bba8e9cf57
Alberto Spadoni <80253259+alberto-spadoni3@users.noreply.github.com>
2024-05-04 16:03:47 +0200

    Rimosso companion object in quanto ridondante
    Le case class offrono di default un companion object provvisto di metodo apply

--- src/main/scala/it/unibo/pps/model/Report.scala
+++ src/main/scala/it/unibo/pps/model/Report.scala
@@ -9,21 +9,3 @@ package it.unibo.pps.model
   *   Punti dell'avversario in quella partita
   */
 case class Report(adversaryName: String, playerPoints: Int, adversaryPoints: Int)
-
-/** Companion object che funge da factory per la classe [[Report]].
-  */
-object Report {
-
-  /** Crea un oggetto di tipo [[Report]].
-    * @param adversaryName
-    *   Nome dell'utente oggetto della statistica
-    * @param playerPoints
-    *   Punti dell'utente in una partita
-    * @param adversaryPoints
-    *   Punti dell'avversario in una partita
-    * @return
-    *   Oggetto di tipo [[Report]]
-    */
-  def apply(adversaryName: String, playerPoints: Int, adversaryPoints: Int): Report =
-    new Report(adversaryName, playerPoints, adversaryPoints)
-}
